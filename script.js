a4eacd517b6f1dff8a82c5796db53dd385d71e27
Atonu Roy Chowdhury <166700173+atonu-a@users.noreply.github.com>
2026-02-05 23:44:10 +0600

    Update script.js

--- script.js
+++ script.js
@@ -17,4 +17,36 @@ themeBtn.addEventListener("click", () => {
         themeBtn.innerText = "☾ Dark Mode";
     }
   
+
+});
+
+
+
+const menuWrapper = document.querySelector('.menu.group');
+const menuDropdown = menuWrapper.querySelector('.ul-menu');
+const menuArrow = menuWrapper.querySelector('span');
+
+menuWrapper.addEventListener('click', function(e) {
+    
+    if (e.target.tagName === 'A' || e.target.tagName === 'BUTTON') {
+        menuDropdown.style.maxHeight = null;
+        menuArrow.classList.remove('rotate-90');
+        return;
+    }
+
+
+    if (menuDropdown.style.maxHeight === '500px') {
+        menuDropdown.style.maxHeight = null;
+        menuArrow.classList.remove('rotate-90');
+    } else {
+        menuDropdown.style.maxHeight = '500px';
+        menuArrow.classList.add('rotate-90');
+    }
+});
+
+document.addEventListener('click', function(e) {
+    if (!menuWrapper.contains(e.target)) {
+        menuDropdown.style.maxHeight = null;
+        menuArrow.classList.remove('rotate-90');
+    }
 });
